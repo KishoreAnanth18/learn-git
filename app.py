288f83b25392ee187892c8623df07f612d2c577d
KishoreAnanth18 <kishoreananth.it20@bitsathy.ac.in>
2023-03-05 12:18:33 +0530

    commit for fb2

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/fbranch1')
+@app.route('/fbranch2')
 def fbranch1():
-    return render_template('fbranch1.html')
+    return render_template('fbranch2.html')
 
 if __name__=="__main__":
     app.run(debug=True)
