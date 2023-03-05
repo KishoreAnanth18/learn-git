f04fc6d7fe519dc8696e05c3e45501598b02375e
KishoreAnanth18 <kishoreananth.it20@bitsathy.ac.in>
2023-03-05 15:04:12 +0530

    redefined fbranch1

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/fbranch2')
+@app.route('/fbranch1')
 def fbranch1():
-    return render_template('fbranch2.html')
+    return render_template('fbranch1.html')
 
 if __name__=="__main__":
     app.run(debug=True)
